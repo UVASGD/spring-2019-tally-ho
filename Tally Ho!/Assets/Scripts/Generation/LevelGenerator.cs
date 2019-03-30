3670f1fa44be4c4012d3bcdd89b59ad7a184abd4
CaliberSeven <echen104@gmail.com>
2019-03-29 23:36:47 -0400

    Level Generation Works
    It just does.

--- Tally Ho!/Assets/Scripts/Generation/LevelGenerator.cs
+++ Tally Ho!/Assets/Scripts/Generation/LevelGenerator.cs
@@ -8,9 +8,10 @@ public class LevelGenerator : MonoBehaviour {
     public GameObject RoomPrefab;
     public GameObject Player;
     public float roomWidth, roomHeight;
+    public float randomRoomChance;
 
     private void Awake() {
-        Room[,] rooms = GenerateLayout(levelwidth, levelheight, 0);
+        List<Room> rooms = GenerateLayout(levelwidth, levelheight, 0);
         roomWidth = RoomPrefab.transform.Find("Background").localScale.x - RoomPrefab.transform.Find("WallTopRight").localScale.y;
         roomHeight = RoomPrefab.transform.Find("Background").localScale.y - RoomPrefab.transform.Find("WallTopRight").localScale.y;
         PutRoomsInScene(rooms);
@@ -29,24 +30,24 @@ public class LevelGenerator : MonoBehaviour {
         Debug.Log(buffer);
     }
 
-    public Room[,] GenerateLayout(int width, int height, int minRooms) {
+    public List<Room> GenerateLayout(int width, int height, int minRooms) {
 
         Room[,] grid = new Room[width, height];
         for (int i = 0; i < grid.GetLength(0); i++) {
             for (int j = 0; j < grid.GetLength(1); j++) {
-                grid[i, j] = new Room();
+                grid[i, j] = new Room(_x : i, _y : j);
             }
         }
-        List<Room> startingPathRooms = new List<Room>();//contains all rooms in the initial path including start and end
+        //add random rooms
+        RandomRooms(grid, randomRoomChance);
+
 
         int startIndex = Random.Range(0, height);
 
-        grid[0, startIndex] = new Room(_type: RoomType.start);
-        startingPathRooms.Add(grid[0, startIndex]);
+        grid[0, startIndex].type = RoomType.start;
 
         int endIndex = Random.Range(0, height);
 
-        grid[width - 1, endIndex] = new Room(_type: RoomType.end);
 
         //create shortest path for initial sequence of rooms
         int x = 0;
@@ -56,36 +57,27 @@ public class LevelGenerator : MonoBehaviour {
             if (y == endIndex) {
                 //if same level as end room, always go right
                 x++;
-                grid[x - 1, y].right = DoorState.open;
-                grid[x, y] = new Room(_type: RoomType.normal, _left: DoorState.open);
-                startingPathRooms.Add(grid[x, y]);
+                grid[x, y].type = RoomType.normal;
             } else if (y > endIndex) {
                 //if below end room, randomly decide between up and right
                 int coinflip = Random.Range(0, 3);
                 if (coinflip != 0) {
                     x++;
-                    grid[x - 1, y].right = DoorState.open;
-                    grid[x, y] = new Room(_type: RoomType.normal, _left: DoorState.open);
-                    startingPathRooms.Add(grid[x, y]);
+                    grid[x, y].type = RoomType.normal;
                 } else {
                     y--;
-                    grid[x, y + 1].up = DoorState.open;
-                    grid[x, y] = new Room(_type: RoomType.normal, _down: DoorState.open);
-                    startingPathRooms.Add(grid[x, y]);
+                    grid[x, y].type = RoomType.normal;
                 }
             } else {
                 //if above end room, randomly decide between down and right
                 int coinflip = Random.Range(0, 3);
                 if (coinflip != 0) {
                     x++;
-                    grid[x - 1, y].right = DoorState.open;
-                    grid[x, y] = new Room(_type: RoomType.normal, _left: DoorState.open);
-                    startingPathRooms.Add(grid[x, y]);
-                } else {
+                    grid[x, y].type = RoomType.normal;
+                } else
+                {
                     y++;
-                    grid[x, y - 1].down = DoorState.open;
-                    grid[x, y] = new Room(_type: RoomType.normal, _up: DoorState.open);
-                    startingPathRooms.Add(grid[x, y]);
+                    grid[x, y].type = RoomType.normal;
                 }
             }
         }
@@ -93,36 +85,93 @@ public class LevelGenerator : MonoBehaviour {
         while (y != endIndex) {
             if (y < endIndex) {
                 y++;
-                grid[x, y - 1].down = DoorState.open;
-                grid[x, y] = new Room(_type: RoomType.normal, _up: DoorState.open);
-                startingPathRooms.Add(grid[x, y]);
+                grid[x, y].type = RoomType.normal;
             } else {
                 y--;
-                grid[x, y + 1].up = DoorState.open;
-                grid[x, y] = new Room(_type: RoomType.normal, _down: DoorState.open);
-                startingPathRooms.Add(grid[x, y]);
+                grid[x, y].type = RoomType.normal;
             }
         }
         grid[width - 1, endIndex].type = RoomType.end;
 
-        //add random rooms
+        //BFS from start room
+        List<Room> floorRooms = new List<Room>();//contains all rooms in the floor
+       //floorRooms.Add(grid[0, startIndex]);
+        Queue<Room> q = new Queue<Room>();
+        q.Enqueue(grid[0, startIndex]);
+        while(q.Count > 0)
+        {
+            Room nextRoom = q.Dequeue();
+            nextRoom.reached = true;
+            floorRooms.Add(nextRoom);
+            foreach(Room r in GetNeighbors(nextRoom, grid)){
+                if(r.type != RoomType.none && !r.reached)
+                {
+                    q.Enqueue(r);
+                }
+            }
+        }
 
 
-        return grid;
+        //open appropriate doors
+        foreach(Room r in floorRooms)
+        {
+            if(r.x > 0 && grid[r.x - 1, r.y].type != RoomType.none)
+            {
+                r.left = DoorState.open;
+                grid[r.x - 1, r.y].right = DoorState.open;
+            }
+            if (r.y > 0 && grid[r.x, r.y - 1].type != RoomType.none)
+            {
+                r.up = DoorState.open;
+                grid[r.x, r.y - 1].down = DoorState.open;
+            }
+            if (r.x < grid.GetLength(0) - 1 && grid[r.x + 1, r.y].type != RoomType.none)
+            {
+                r.right = DoorState.open;
+                grid[r.x + 1, r.y].left = DoorState.open;
+            }
+            if (r.y < grid.GetLength(1) - 1 && grid[r.x, r.y + 1].type != RoomType.none)
+            {
+                r.down = DoorState.open;
+                grid[r.x, r.y + 1].up = DoorState.open;
+            }
+        }
+        return floorRooms;
     }
 
-    void PutRoomsInScene(Room[,] grid) {
+    List<Room> GetNeighbors(Room r, Room[,] grid)
+    {
+        List<Room> result = new List<Room>();
+        if(r.x > 0)
+        {
+            result.Add(grid[r.x - 1, r.y]);
+        }
+        if(r.y > 0)
+        {
+            result.Add(grid[r.x, r.y - 1]);
+        }
+        if (r.x < grid.GetLength(0)-1)
+        {
+            result.Add(grid[r.x + 1, r.y]);
+        }
+        if (r.y < grid.GetLength(1)-1)
+        {
+            result.Add(grid[r.x, r.y + 1]);
+        }
+        return result;
+    }
 
-        for (int i = 0; i < grid.GetLength(0); i++) {
-            for (int j = 0; j < grid.GetLength(1); j++) {
-                if(grid[i,j].type != RoomType.none)
-                {
-                    addRoomAtPosition(i, j, grid[i,j]);
-                }
+    void PutRoomsInScene(List<Room> desiredRooms) {
+        foreach (Room r in desiredRooms)
+        {
+            if (r.type != RoomType.none)
+            {
+                addRoomAtPosition(r.x, r.y, r);
             }
         }
     }
 
+    //Adds Rooms and Doors
     void addRoomAtPosition(int i, int j, Room room) {
         Vector3 pos = new Vector3(transform.position.x + i * roomWidth, transform.position.y - j * roomHeight, transform.position.z);
         GameObject newRoom = Instantiate(RoomPrefab, pos, Quaternion.identity, transform);
@@ -168,6 +217,20 @@ public class LevelGenerator : MonoBehaviour {
             Destroy(newRoom.transform.Find("WallRightBottom").gameObject);
         }
     }
-
-
+    
+    //Runs Before Generate Layout
+    void RandomRooms(Room[,] grid, float chance)
+    {
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                float coinflip = Random.Range(0f, 1f);
+                if (coinflip < chance)
+                {
+                    grid[i, j].type = RoomType.normal;
+                }
+            }
+        }
+    }
 }
