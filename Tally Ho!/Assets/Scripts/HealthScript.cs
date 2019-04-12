6a9b05bcfbaa3171267a0179c94d6ec90b83c695
seemspromising <43363614+seemspromising@users.noreply.github.com>
2019-04-12 16:22:45 -0400

    baby enamie

---
+++ Tally Ho!/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigCollider)
+    {
+        if (trigCollider.gameObject.tag == "player attack")
+        {
+            health -= 10;
+        }
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
